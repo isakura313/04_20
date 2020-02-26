3b7c1243a477982d5e9a39dc0b9fbb1bad109981
xZarathustrAx <60914636+xZarathustrAx@users.noreply.github.com>
2020-02-26 22:28:04 +0300

    slyder

---
+++ js/code.js
@@ -0,0 +1,28 @@
+// alert('привет! как дела!');
+let button = document.querySelector(".promo__button");
+let wrap_form = document.querySelector(".wrap_form");
+let cross = document.querySelector("i[class='fa fa-times']");
+let faq_panel = document.querySelector(".faq_panel");
+let faq_answer = document.querySelector(".faq_answer");
+
+
+// задача чтобы у нас при клике на панель становился видимым блок ответа
+// не прошу что бы обратное действие
+
+button.onclick = function(){
+    wrap_form.style.display = "flex";
+}
+cross.onclick = function(){
+    wrap_form.classList.add("zoomOut");
+
+    setTimeout(() => {
+        wrap_form.style.display = "none";
+        wrap_form.classList.remove("zoomOut");
+    }, 1000);
+   
+}
+faq_panel.onclick =function(){
+faq_answer.style.display = "flex"
+
+
+}
